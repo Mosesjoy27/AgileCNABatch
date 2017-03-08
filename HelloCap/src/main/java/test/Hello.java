8306dd6068cb9d333084e9366da9bae24b493a5d
Chinta <chinta.prasanthjoy@capgemini.com>
2017-03-08 17:01:48 +0530

    Hai Project

---
+++ HelloCap/src/main/java/test/Hello.java
@@ -0,0 +1,9 @@
+package test;
+
+public class Hai
+{
+    public static void main(String[] args)
+     { 
+        System.out.println("Saying Hai from the new piece of code which i need to upload");
+     }
+}
